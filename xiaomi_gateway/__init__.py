c539346e43041d7e2dd3a0ed9c12a4d8a3c1c1cc
luci-11 <luci-11@users.noreply.github.com>
2018-06-12 15:28:50 +0200

    add missing import

--- xiaomi_gateway/__init__.py
+++ xiaomi_gateway/__init__.py
@@ -8,6 +8,7 @@ from collections import defaultdict
 from threading import Thread
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
+import time
 
 _LOGGER = logging.getLogger(__name__)
 
